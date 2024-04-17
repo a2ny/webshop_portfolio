f2f4d5b0ea80532e38675fd083bf9f8692a6c358
a2ny <143804387+a2ny@users.noreply.github.com>
2024-04-17 21:12:06 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,10 +20,7 @@ app.use(
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://webshop-portfolio-api.vercel.app",
-    ],
+    origin: ["https://webshop-portfolio-api.vercel.app"],
     method: ["POST", "GET"],
     credentials: true,
   })
