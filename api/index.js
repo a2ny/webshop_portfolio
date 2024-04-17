6f4ad0d0a0df1ebf23f023ae4e7f494a25cdbedd
a2ny <143804387+a2ny@users.noreply.github.com>
2024-04-17 22:08:15 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -25,6 +25,10 @@ app.get("/", cors(), (req, res) => {
 });
 
 // Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 ///////////////////////////////////////////////////////////////////////////////////
 // Signup response
